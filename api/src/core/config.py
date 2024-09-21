7c38a019e6fbf6a4f918dd7c26681f0395b3184c
Maxim Zhdanov <exodus.yarwood@icloud.com>
2024-09-21 22:54:17 +0300

    Новый принцип указания расположения директории с изображениями

--- api/src/core/config.py
+++ api/src/core/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     PG_DSN: PostgresDsn = Field(alias="API_PG_DSN")
     REDIS_DSN: RedisDsn
     REDIS_STREAM_TO_IMAGE_HANDLER_NAME: str
+    MEDIA_ROOT: str
 
     class Config:
         env_file = '.env'
